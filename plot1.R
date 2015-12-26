0e886c5cba13e878614bb4d57031115699e5f8a5
thegeolawton <geofry.m.lawton@gmail.com>
2015-12-26 10:00:35 -0500

    Added explanatory comments...

--- plot1.R
+++ plot1.R
@@ -1,13 +1,15 @@
-#NEI <- readRDS("summarySCC_PM25.rds")
+#use the Base R Graphics system to show whether total US PM2.5 emissions have gone down since 1999
+
+NEI <- readRDS("summarySCC_PM25.rds")
 library(dplyr)
-q1 <- group_by(NEI,year) %>% summarise(Total = sum(Emissions))
+q1 <- group_by(NEI,year) %>% summarise(Total = sum(Emissions)) 
 plot(q1,main = "Total US Pollution", xlab = "Year", ylab = "Total Emissions")
-lines(q1)
-beg <- NEI[NEI$year == min(NEI$year),]
-end <- NEI[NEI$year == max(NEI$year),]
+lines(q1) #connect dots
+beg <- NEI[NEI$year == min(NEI$year),] #subset for beginning pm2.5 value
+end <- NEI[NEI$year == max(NEI$year),] #subset for ending pm2.5 value
 #points(cbind(sum(beg$Emissions),unique(NEI$year)),col="red")
-lines(cbind(unique(NEI$year),sum(beg$Emissions)),col="red")
+lines(cbind(unique(NEI$year),sum(beg$Emissions)),col="red") #Draw line and text for beg value
 text(x= max(NEI$year), y = sum(.95*beg$Emissions), paste(min(NEI$year),"emissions:", format(sum(beg$Emissions), digits=5, big.mark = "," , big.interval = 3)), pos = 2, cex = .60, col = "red")
-lines(cbind(unique(NEI$year),sum(end$Emissions)),col="green")
+lines(cbind(unique(NEI$year),sum(end$Emissions)),col="green") #Draw line and text for end value
 text(x= max(NEI$year), y = sum(1.05*end$Emissions), paste(max(NEI$year),"emissions:", format(sum(end$Emissions), digits=5, big.mark = "," , big.interval = 3)), pos = 2, cex = .60, col = "green")
 dev.print(png,filename = "plot1.png", width = 600, height = 400)
